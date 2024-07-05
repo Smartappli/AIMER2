5930475571a3bf4a17bdd6ae4bb8497e98e9245e
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-07-05 06:58:31 +0200

    improvement of blog

--- blog/models.py
+++ blog/models.py
@@ -17,11 +17,14 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='posts',
+        related_name='blog_posts'
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -29,11 +32,12 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT,
+        choices=Status,
+        default=Status.DRAFT
     )
-    objects = models.Manager()
-    published = PublishedManager()
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
@@ -47,5 +51,33 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse(
             'blog:post_detail',
-            args=[self.id]
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
