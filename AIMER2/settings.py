7bcdaaeb6f833d7a2ef69e3810fef1c5185e2426
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-07-05 08:38:11 +0200

    Linters

--- AIMER2/settings.py
+++ AIMER2/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
