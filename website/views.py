78a29234917684f8312fc4793df8b5818f645c33
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-07-07 10:05:41 +0200

    Linters

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from .forms import LoginForm, UserRegistrationForm
 
 
