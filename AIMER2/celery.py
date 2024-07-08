d6ff4d83ae7ca1d06cbe6be2f76ac37de9d3f255
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-07-08 16:30:28 +0200

    add celery on all apps

--- AIMER2/celery.py
+++ AIMER2/celery.py
@@ -1,9 +1,15 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIMER2.settings')
 
 app = Celery('AIMER2')
+
+# Configure Celery using settings from Django settings.py.
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+
+# Load tasks from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
