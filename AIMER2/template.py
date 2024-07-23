1527d5c1537883e109d7347ad6069cc9c8e522e3
Smartappli <26173534+Smartappli@users.noreply.github.com>
2024-07-23 13:36:54 +0000

    :art: Format Python code with psf/black

--- AIMER2/template.py
+++ AIMER2/template.py
@@ -7,25 +7,25 @@
 # Template config ? Easily change the template configuration from here ? Replace this object with
 # template-config/demo-*.py file's TEMPLATE_CONFIG to change the template configuration as per our demos
 TEMPLATE_CONFIG = {
-    "layout": "vertical",             # Options[String]: vertical(default), horizontal
-    "theme": "theme-semi-dark",       # Options[String]: theme-default(default), theme-bordered, theme-semi-dark
-    "style": "light",                 # Options[String]: light(default), dark, system mode
-    "rtl_support": True,              # options[Boolean]: True(default), False # To provide RTLSupport or not
-    "rtl_mode": False,                # options[Boolean]: False(default), True # To set layout to RTL layout  (
-                                      # myRTLSupport must be True for rtl mode)
-    "has_customizer": True,           # options[Boolean]: True(default), False # Display customizer or not THIS WILL
-                                      # REMOVE INCLUDED JS FILE. SO LOCAL STORAGE WON'T WORK
-    "display_customizer": True,       # options[Boolean]: True(default), False # Display customizer UI or not,
-                                      # THIS WON'T REMOVE INCLUDED JS FILE. SO LOCAL STORAGE WILL WORK
-    "content_layout": "compact",      # options[String]: 'compact', 'wide' (compact=container-xxl, wide=container-fluid)
-    "navbar_type": "fixed",           # options[String]: 'fixed', 'static', 'hidden' (Only for vertical Layout)
-    "header_type": "fixed",           # options[String]: 'static', 'fixed' (for horizontal layout only)
-    "menu_fixed": True,               # options[Boolean]: True(default), False # Layout(menu) Fixed (Only for
-                                      # vertical Layout)
-    "menu_collapsed": False,          # options[Boolean]: False(default), True # Show menu collapsed, Only for
-                                      # vertical Layout
-    "footer_fixed": False,            # options[Boolean]: False(default), True # Footer Fixed
-    "show_dropdown_onhover": True,    # True, False (for horizontal layout only)
+    "layout": "vertical",  # Options[String]: vertical(default), horizontal
+    "theme": "theme-semi-dark",  # Options[String]: theme-default(default), theme-bordered, theme-semi-dark
+    "style": "light",  # Options[String]: light(default), dark, system mode
+    "rtl_support": True,  # options[Boolean]: True(default), False # To provide RTLSupport or not
+    "rtl_mode": False,  # options[Boolean]: False(default), True # To set layout to RTL layout  (
+    # myRTLSupport must be True for rtl mode)
+    "has_customizer": True,  # options[Boolean]: True(default), False # Display customizer or not THIS WILL
+    # REMOVE INCLUDED JS FILE. SO LOCAL STORAGE WON'T WORK
+    "display_customizer": True,  # options[Boolean]: True(default), False # Display customizer UI or not,
+    # THIS WON'T REMOVE INCLUDED JS FILE. SO LOCAL STORAGE WILL WORK
+    "content_layout": "compact",  # options[String]: 'compact', 'wide' (compact=container-xxl, wide=container-fluid)
+    "navbar_type": "fixed",  # options[String]: 'fixed', 'static', 'hidden' (Only for vertical Layout)
+    "header_type": "fixed",  # options[String]: 'static', 'fixed' (for horizontal layout only)
+    "menu_fixed": True,  # options[Boolean]: True(default), False # Layout(menu) Fixed (Only for
+    # vertical Layout)
+    "menu_collapsed": False,  # options[Boolean]: False(default), True # Show menu collapsed, Only for
+    # vertical Layout
+    "footer_fixed": False,  # options[Boolean]: False(default), True # Footer Fixed
+    "show_dropdown_onhover": True,  # True, False (for horizontal layout only)
     "customizer_controls": [
         "rtl",
         "style",
@@ -48,9 +48,9 @@ THEME_VARIABLES = {
     "template_version": "2.0.0",
     "template_free": False,
     "template_description": "Sneat is a modern, clean and fully responsive admin template built with Bootstrap 5, "
-                            "Django, HTML, CSS, and JavaScript. It has a huge collection of reusable UI components. "
-                            "It can be used for all types of web applications like custom admin panel, "
-                            "project management system, admin dashboard, Backend application or CRM.",
+    "Django, HTML, CSS, and JavaScript. It has a huge collection of reusable UI components. "
+    "It can be used for all types of web applications like custom admin panel, "
+    "project management system, admin dashboard, Backend application or CRM.",
     "template_keyword": "django, django admin, dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5",
     "facebook_url": "https://www.facebook.com/ThemeSelections/",
     "twitter_url": "https://twitter.com/Theme_Selection",
