150637fcb24e123168863e49c624f3cdae6354f2
Smartappli <26173534+Smartappli@users.noreply.github.com>
2024-07-24 18:13:38 +0000

    :art: Format Python code with psf/black

--- AIMER2/urls.py
+++ AIMER2/urls.py
@@ -57,14 +57,18 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = SystemView.as_view(
-    template_name="pages/pages_misc_error.html", status=404,
+    template_name="pages/pages_misc_error.html",
+    status=404,
 )
 handler403 = SystemView.as_view(
-    template_name="pages/pages_misc_not_authorized.html", status=403,
+    template_name="pages/pages_misc_not_authorized.html",
+    status=403,
 )
 handler400 = SystemView.as_view(
-    template_name="pages/pages_misc_error.html", status=400,
+    template_name="pages/pages_misc_error.html",
+    status=400,
 )
 handler500 = SystemView.as_view(
-    template_name="pages/pages_misc_error.html", status=500,
+    template_name="pages/pages_misc_error.html",
+    status=500,
 )
