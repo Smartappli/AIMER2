881c93f20314c57e6cb739725ac77ce2326c40ca
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-08-10 22:49:26 +0200

    Update llama_cpp.py

--- ai_models/llama_cpp.py
+++ ai_models/llama_cpp.py
@@ -2,6 +2,6 @@ from llama_cpp import Llama
 
 llm = Llama.from_pretrained(
     repo_id="Qwen/Qwen2-0.5B-Instruct-GGUF",
-    filename="*q8_0.gguf",
+    filename="*q4_K_M.gguf",
     verbose=True
 )
