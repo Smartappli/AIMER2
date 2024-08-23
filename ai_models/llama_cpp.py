81d3421e03777bb3422384fadd3164a2f9ce66d4
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-08-23 13:20:27 +0200

    Update llama_cpp.py

--- ai_models/llama_cpp.py
+++ ai_models/llama_cpp.py
@@ -3,12 +3,15 @@ from llama_cpp import Llama
 llm = Llama.from_pretrained(
     repo_id="Qwen/Qwen2-0.5B-Instruct-GGUF",
     filename="*q4_K_M.gguf",
-    verbose=True
+    verbose=True,
 )
 output = llm(
-      "Q: Name the planets in the solar system? A: ", # Prompt
-      max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
-      echo=True # Echo the prompt back in the output
-) # Generate a completion, can also call create_completion
+    "Q: Name the planets in the solar system? A: ",  # Prompt
+    max_tokens=32,  # Generate up to 32 tokens, set to None to generate up to the end of the context window
+    stop=[
+        "Q:",
+        "\n",
+    ],  # Stop generating just before the model would generate a new question
+    echo=True,  # Echo the prompt back in the output
+)  # Generate a completion, can also call create_completion
 print(output)  # noqa
