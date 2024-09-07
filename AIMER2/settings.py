8153ad902b6455840079c85fda85135897469446
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-09-08 01:58:52 +0200

    update page account settings

--- AIMER2/settings.py
+++ AIMER2/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
+    "django_countries",
     "embed_video",
     # "faq.apps.FaqConfig",
     "pages.apps.PagesConfig",
